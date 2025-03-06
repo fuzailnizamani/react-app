d4384a5a7ee3baece2c1b2254a45052e5a275e6f
fuzailnizmani <fuzailnizamani13@gmail.com>
2025-03-06 23:33:40 +0500

    added starting code

---
+++ src/Message.jsx
@@ -0,0 +1,6 @@
+
+function Message(){
+  return <h1>HEllo World</h1>
+}
+
+export default Message; 
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+
+createRoot(document.querySelector("#root")).render(
+<ul>
+  <li>because i want build website</li>
+  <li>i want to create fast website</li>
+  <li>because i want get a job </li>
+</ul>
+);
